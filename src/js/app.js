241a3f2d9f67e9016f82f0d5df865651b925fe73
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-02-12 23:52:31 +0000

    created quiz and applied styling and js animations

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,16 @@ import './modules/helpers';
 // Quiz
 const correctAnswers = [ 'B', 'B', 'B', 'B' ];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.score');
+const resultContent = document.querySelector('.score__content');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	let score = 0;
 	const userAnswers = [ form.q1.value, form.q2.value, form.q3.value, form.q4.value ];
+	const resultNum = resultContent.querySelector('.score__number');
+	const resultTxt = resultContent.querySelector('h3');
 
 	// Checks user answers with correct ones.
 	userAnswers.forEach((answer, index) => {
@@ -21,5 +25,47 @@ form.addEventListener('submit', (e) => {
 		}
 	});
 
-	console.log(score);
+	scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+
+	if (score <= 0 && result.classList.contains('score--expanded')) {
+		resultContent.style.opacity = '0';
+		setTimeout(() => {
+			result.style.maxHeight = '0px';
+			result.classList.remove('score--expanded');
+			result.setAttribute('aria-expanded', 'false');
+			setTimeout(() => {
+				resultContent.style.display = 'none';
+				result.style.display = 'none';
+			}, 300);
+		}, 200);
+	} else {
+		if (!result.classList.contains('score--expanded')) {
+			result.style.display = 'block';
+			result.style.maxHeight = '69px';
+			result.classList.add('score--expanded');
+			result.setAttribute('aria-expanded', 'true');
+			let height = Math.ceil(result.offsetHeight + 100) + 'px';
+			result.style.maxHeight = '0px';
+			setTimeout(() => {
+				resultContent.style.display = 'block';
+				result.style.maxHeight = height;
+				setTimeout(() => {
+					resultContent.style.opacity = '1';
+				}, 300);
+			}, 0);
+		}
+
+		let output = 0;
+		const timer = setInterval(() => {
+			resultNum.textContent = `${output}%`;
+			if (output === score) {
+				clearInterval(timer);
+			} else {
+				output++;
+			}
+		}, 55);
+	}
 });
